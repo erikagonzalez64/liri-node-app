585eb0f255e7d804ae1cd5072dff4cb2726a76be
erika gonzalez <gonzalezerika75951@gmail.com>
2019-10-03 22:34:30 -0500

    updated my liri.js for some basic things that i needed to add in, but still working on it.

--- liri.js
+++ liri.js
@@ -1,3 +1,18 @@
+//info for spotify
 require("dotenv").config();
-
+// import the node-spotify-api NPM package
+var Spotify = require("node-spotify-api");
+//import the API keys
 var keys = require("./keys.js");
+//initializing the spotify API client with keys
+var spotify = new Spotify(keys.spotify);
+//info for request
+var request = require("request");
+//info for moment
+var moment = require("moment");
+//info for fs
+var fs = require("fs");
+//it takes command
+var command = process.argv[2];
+
+
